e4fe1c0eecfa9d96cbd1433ea203636de7a66753
unknown <rafagonpin317@gmail.com>
2024-10-14 15:02:47 -0300

    inserindo dados do candidato

--- src/main/java/org/codedontblow/Main.java
+++ src/main/java/org/codedontblow/Main.java
@@ -21,8 +21,8 @@ public class Main {
         //Inserção de dados do output dentro do banco de dados
 
         // Inserir Boletim
-        InsertSQL.insereOutput("12,Prof Juliana,9.0,8.0/13,Prof Josias,7.6,5.5/14,Prof Maria José,10.0,6.5", "Boletim");
+        InsertSQL.insereOutput("Aurora,12,Escola Prof. Juliana,9.0,8.0/Renata13,Escola Prof. Josias,7.6,5.5/Gabriel,14,Escola Prof. Maria José,10.0,6.5", "Boletim");
         // Inserir Curriculo
-        InsertSQL.insereOutput("99612345,carlos@gmail.com,@carlos123,carlos.com,R. Carlos,Programação,Portugues/99418949,ana@gmail.com,@ana123,ana.com,R. Ana,Marketing,Inglês" , "Curriculo");
+        InsertSQL.insereOutput("Carlos,99612345,carlos@gmail.com,@carlos123,carlos.com,R. Carlos,Programação,Portugues/Ana,99418949,ana@gmail.com,@ana123,ana.com,R. Ana,Marketing,Inglês" , "Curriculo");
     }
 }
--- src/main/java/org/codedontblow/dao/InsertSQL.java
+++ src/main/java/org/codedontblow/dao/InsertSQL.java
@@ -5,17 +5,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-//!!! FALTA INSERIR OS DADOS DO CANDIDATO !!!
-public class InsertSQL {
+//!!! FALTA RELACIONAR OS ID'S !!!
+public class InsertSQL{
 
     //Recebe como parâmetros de entrada o output fornecido pelo moondream e o tipo de documento que será cadastrado
     public static void insereOutput(String moondreamOutput, String tipoDoc) throws SQLException {
 
-        // Inicia uma conexão com o Banco de Dados e a chama de "conn"
+        //Inicia uma conexão com o Banco de Dados e a chama de "conn"
         Connection conn = new ConnectionFactory().getConnection();
 
         //Define o código SQL para inserção na tabela "Candidato"
         String sqlCandidato = "INSERT INTO candidato (nome, tipo_doc) VALUES (?, ?)";
+        PreparedStatement stmtCandidato = conn.prepareStatement(sqlCandidato);
 
         //Define o código SQL para inserção na tabela "Boletim"
         String sqlBoletim = "INSERT INTO boletim (matricula, escola, nota_port, nota_mat) VALUES (?, ?, ?, ?)";
@@ -25,8 +26,6 @@ public class InsertSQL {
         String sqlCurriculo = "INSERT INTO curriculo (telefone, email, linkedin, portifolio, endereco, competencias, idiomas) VALUES (?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement stmtCurriculo = conn.prepareStatement(sqlCurriculo);
 
-
-
         try {
             //Tratamento do output do Moondream. Separando as linhas
             String[] linhas = moondreamOutput.split("/");
@@ -35,29 +34,38 @@ public class InsertSQL {
             for (String linha : linhas) {
                 String[] atributos = linha.split(",");
 
+                stmtCandidato.setString(1, atributos[0]);
+                stmtCandidato.setString(2, tipoDoc);
+                stmtCandidato.addBatch();
+
                 //Criando o código SQL para inserir todos os atributos presentes no output dentro da tabela boletim, caso o tipo de documento seja um boletim.
                 if (tipoDoc.equals("Boletim")) {
-                    stmtBoletim.setString(1, atributos[0]); //Número da Matrícula
-                    stmtBoletim.setString(2, atributos[1]); //Nome da Escola
-                    stmtBoletim.setString(3, atributos[2]); //Nota de Português
-                    stmtBoletim.setString(4, atributos[3]); //Nota de Matemática
+
+                    stmtBoletim.setString(1, atributos[1]); //Número da Matrícula
+                    stmtBoletim.setString(2, atributos[2]); //Nome da Escola
+                    stmtBoletim.setString(3, atributos[3]); //Nota de Português
+                    stmtBoletim.setString(4, atributos[4]); //Nota de Matemática
                     stmtBoletim.addBatch(); //Armazena os comandos sql em um lote que será executado em breve
                 }
 
                 //Criando o código SQL para inserir todos os atributos presentes no output dentro da tabela curriculo, caso o tipo de documento seja um currículo.
                 else if (tipoDoc.equals("Curriculo")){
-                    stmtCurriculo.setString(1, atributos[0]); //telefone
-                    stmtCurriculo.setString(2, atributos[1]); //email
-                    stmtCurriculo.setString(3, atributos[2]); //linkedin
-                    stmtCurriculo.setString(4, atributos[3]); //portifolio
-                    stmtCurriculo.setString(5, atributos[4]); //endereço
-                    stmtCurriculo.setString(6, atributos[5]); //competencias
-                    stmtCurriculo.setString(7, atributos[6]); //idiomas
+                    stmtCurriculo.setString(1, atributos[1]); //telefone
+                    stmtCurriculo.setString(2, atributos[2]); //email
+                    stmtCurriculo.setString(3, atributos[3]); //linkedin
+                    stmtCurriculo.setString(4, atributos[4]); //portifolio
+                    stmtCurriculo.setString(5, atributos[5]); //endereço
+                    stmtCurriculo.setString(6, atributos[6]); //competencias
+                    stmtCurriculo.setString(7, atributos[7]); //idiomas
                     stmtCurriculo.addBatch(); //Armazena os comandos sql em um lote que será executado em breve
                 }
             }
 
             //Inserindo no Banco de Dados
+
+            stmtCandidato.executeBatch();
+            stmtCandidato.close();
+            System.out.println("Candidato Cadastrado!");
 
             //Executa os códigos SQL, armazenados no lote, referentes a tabela boletim
             if (tipoDoc.equals("Boletim")) {
@@ -72,7 +80,6 @@ public class InsertSQL {
                 stmtCurriculo.close();
                 System.out.println("Inserção de Curriculo Bem Sucedida!");
             }
-
         }
 
         catch(SQLException u) {
