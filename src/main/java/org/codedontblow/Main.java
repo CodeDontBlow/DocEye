af2435427618c054134d57a5fd8d84fb0f147e74
Leonardo-dSouza <dkaireturn@gmail.com>
2024-10-21 21:19:16 -0300

    mergeando read para a main

--- src/main/java/org/codedontblow/Main.java
+++ src/main/java/org/codedontblow/Main.java
@@ -1,23 +1,26 @@
 package org.codedontblow;
 
 import io.github.ollama4j.exceptions.OllamaBaseException;
-import org.codedontblow.dao.InsertSQL;
 import org.codedontblow.services.InputDocuments;
 import org.codedontblow.services.OllamaApi;
-import org.codedontblow.services.TesseractOCR;
-
+import org.codedontblow.dao.BoletimDAO;
+import org.codedontblow.dao.CandidatoDAO;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import static org.codedontblow.services.TesseractOCR.processarImagem;
 
 public class Main {
     public static void main(String[] args) throws OllamaBaseException, IOException, InterruptedException, SQLException {
         System.out.println("Hello CodeDontBlow!");
         InputDocuments i1 = new InputDocuments();
         OllamaApi api = new OllamaApi();
+        CandidatoDAO candidatos = new CandidatoDAO();
+        BoletimDAO boletins = new BoletimDAO();
 
         String filePath = i1.selectFile();
         String output = api.processFile(filePath);
+
+        candidatos.ler();
+        boletins.ler();
     }
 }
