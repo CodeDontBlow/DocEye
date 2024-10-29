6102cb9ba1304c59e3e7f113499adf0da3ee5da3
Leonardo-dSouza <dkaireturn@gmail.com>
2024-10-29 09:03:03 -0300

    cfeat - C método de procura do tipo do documento

--- src/main/java/org/codedontblow/Main.java
+++ src/main/java/org/codedontblow/Main.java
@@ -1,26 +1,19 @@
 package org.codedontblow;
 
-import io.github.ollama4j.exceptions.OllamaBaseException;
 import org.codedontblow.services.InputDocuments;
-import org.codedontblow.services.OllamaApi;
-import org.codedontblow.dao.BoletimDAO;
-import org.codedontblow.dao.CandidatoDAO;
-import java.io.IOException;
-import java.sql.SQLException;
 
+import static org.codedontblow.services.TesseractOCR.findType;
+import static org.codedontblow.services.TesseractOCR.processarImagem;
 
 public class Main {
-    public static void main(String[] args) throws OllamaBaseException, IOException, InterruptedException, SQLException {
+    public static void main(String[] args){
         System.out.println("Hello CodeDontBlow!");
         InputDocuments i1 = new InputDocuments();
-        OllamaApi api = new OllamaApi();
-        CandidatoDAO candidatos = new CandidatoDAO();
-        BoletimDAO boletins = new BoletimDAO();
 
         String filePath = i1.selectFile();
-        String output = api.processFile(filePath);
-
-        candidatos.ler();
-        boletins.ler();
+        String text = processarImagem(filePath);  // utiliza o metodo para extrair o texto da imagem
+        String doc = findType(text); // utiliza o metodo para retornar se é boletim ou curriculo
+        // caso não encontra, retorna null.
+        System.out.println(text + doc);
     }
 }
