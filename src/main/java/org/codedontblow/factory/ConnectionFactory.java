e9653f3b1d08160fdd5d99baa5b69e44cb7f2649
Luana Souza <128157430+luanaapms@users.noreply.github.com>
2024-11-14 07:34:13 -0300

    preparo para apresentacao

--- src/main/java/org/codedontblow/factory/ConnectionFactory.java
+++ src/main/java/org/codedontblow/factory/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection getConnection(){
         try{
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/docky", "root", "kali");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/docky", "root", "bdLapms@1711");
         }
         catch(SQLException excecao){
             throw new RuntimeException(excecao);
