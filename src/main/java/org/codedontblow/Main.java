97845a9eb2f72bc43ae6d9d96f07a66c0756e6f5
unknown <rafagonpin317@gmail.com>
2024-11-18 10:45:28 -0300

    Correção de erros referentes ao javafx

--- src/main/java/org/codedontblow/Main.java
+++ src/main/java/org/codedontblow/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application{
 
     public void start(Stage stage) {
         try{
-            Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("TelaInicial.fxml"))));
+            Parent root = FXMLLoader.load(getClass().getResource("/org/codedontblow/TelaInicial.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
 
